4162bfee297a81de818aa4974366c99f3e3a3e99
Radek Lát <radek.lat@gmail.com>
2022-12-29 21:01:47 +0000

    Release 1.0.0

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,29 @@
+from pathlib import Path
+
+import pytest
+import toml
 from _pytest.fixtures import fixture
+from delfino.constants import PYPROJECT_TOML_FILENAME
+from delfino.models import PyprojectToml
 from typer.testing import CliRunner
 
 
 @fixture(scope="session")
 def runner() -> CliRunner:
     return CliRunner()
+
+
+@pytest.fixture(scope="session")
+def project_root():
+    return Path(__file__).parent.parent
+
+
+@pytest.fixture(scope="session")
+def pyproject_toml(project_root):
+    return PyprojectToml(**toml.load(project_root / PYPROJECT_TOML_FILENAME))
+
+
+@pytest.fixture(scope="session")
+def poetry(pyproject_toml):
+    assert pyproject_toml.tool.poetry
+    return pyproject_toml.tool.poetry
